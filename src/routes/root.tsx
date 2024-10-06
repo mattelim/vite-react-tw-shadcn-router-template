51342a257d5b91c6894feb63192e84a206af62b7
Matte Lim <17747414+mattelim@users.noreply.github.com>
2024-10-06 14:19:35 -0400

    Changed <a> to <Link>

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom"
+
 export default function Root() {
   return (
     <>
       <h1>Hello world</h1>
       <p>This is the root page</p>
-      <a href='/test' className="block">Test Link</a>
-      <a href='/9485hg' className="block">Test Error Link</a>
+      <Link to='/test' className="block">Test Link</Link>
+      <Link to='/9485hg' className="block">Test Error Link</Link>
     </>
   )
 }
